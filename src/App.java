e43e6bc4607f6970964c40895e81dea33903d9eb
Victor <victorsmirnov@me.com>
2021-12-05 17:20:04 +0200

    lesson 3

--- src/App.java
+++ src/App.java
@@ -2,31 +2,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Enter operation:");
-        System.out.println("1. Increment");
-        System.out.println("2. Decrement");
-        System.out.println("3. Divide");
-        System.out.println("4. Multiply");
-        Scanner scanner = new Scanner(System.in);
-        int operation = scanner.nextInt();
-        System.out.print("Enter first number: ");
-        Scanner first = new Scanner(System.in);
-        double a = first.nextInt();
-        System.out.print("Enter second number: ");
-        Scanner second = new Scanner(System.in);
-        double b = second.nextInt();
-        double result = 0;
 
-        switch (operation) {
-            case 1: result = a + b; break;
-            case 2: result = a - b; break;
-            case 3: result = a / b; break;
-            default: result = a * b; break;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What number? ");
+        int range = 10;
+        int number = (int) (Math.random() * range);
+
+        while (true) {
+            
+            System.out.println("Enter number from 0 to " + range);
+            int x = scanner.nextInt();
+            if (x == number) {
+                System.out.println("Your number right!");
+                break;
+            } else if (x < number) {
+                System.out.println(x + " is wrong. Number is smaller.");
+            } else {
+                System.out.println(x + " is wrong. Number is bigger.");
+            }
         }
-        System.out.println("Result = " + result);
-        
         scanner.close();
-        first.close();
-        second.close();
     }
 }
