197d7e35742a8b311e1ce97c0f419e3a95506d94
Victor <victorsmirnov@me.com>
2021-12-06 11:52:35 +0200

    lesson 6

---
+++ src/GameWindow.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    private static GameWindow gameWindow;
+    public static void main(String[] args) throws Exception {
+
+        gameWindow = new GameWindow();
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow.setLocation(200, 100);
+        gameWindow.setSize(906, 478);
+        gameWindow.setResizable(false);
+        GameField gameField = new GameField();
+        gameWindow.add(gameField);
+        gameWindow.setVisible(true);
+    }
+
+    private static void onRepaint(Graphics g) {
+        g.fillOval(10, 10, 200, 100);
+        g.drawLine(10, 120, 896, 120);
+    }
+
+    private static class GameField extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            onRepaint(g);
+        }
+    }
+
+}
