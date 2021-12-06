6d840016ab7d72baa0a52879fb5798b3522203f6
Victor <victorsmirnov@me.com>
2021-12-06 12:52:41 +0200

    lesson 7

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,32 +1,52 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
 
     private static GameWindow gameWindow;
-    public static void main(String[] args) throws Exception {
+    private static long lastFrameTime;
+    private static Image background;
+    private static Image gameOver;
+    private static Image drop;
+    private static float dropLeft = 200;
+    private static float dropDown = -100;
+    private static float dropV = 200;
 
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        gameOver = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
         gameWindow = new GameWindow();
+
         gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameWindow.setLocation(200, 100);
         gameWindow.setSize(906, 478);
         gameWindow.setResizable(false);
+        lastFrameTime = System.nanoTime();
         GameField gameField = new GameField();
         gameWindow.add(gameField);
         gameWindow.setVisible(true);
     }
 
     private static void onRepaint(Graphics g) {
-        g.fillOval(10, 10, 200, 100);
-        g.drawLine(10, 120, 896, 120);
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) * 0.000000001f;
+        lastFrameTime = currentTime;
+        g.drawImage(background, 0, 0, null);
+        dropLeft = dropLeft + dropV * deltaTime;
+        dropDown = dropDown + dropV * deltaTime;
+        g.drawImage(drop, (int) dropLeft, (int) dropDown, null);
+        // g.drawImage(gameOver, 290, 120, null);
     }
 
     private static class GameField extends JPanel {
-
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             onRepaint(g);
+            repaint();
         }
     }
 
